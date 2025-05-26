cf937b9d9a70f77cca33ec0bffb20a16e8a1ba21
Victor Saymon <victor.vidal2@aluno.ce.gov.br>
2025-05-26 10:07:13 -0300

    Update conexao.php

--- crud-sy-gijima-Seas/crud-sy-gijima-Seas/gijima-crud-sy-Seas/crud-sy-gijima-Seas/codigos-do-seas/codigos-seas/codigos-php/conexao.php
+++ crud-sy-gijima-Seas/crud-sy-gijima-Seas/gijima-crud-sy-Seas/crud-sy-gijima-Seas/codigos-do-seas/codigos-seas/codigos-php/conexao.php
@@ -7,7 +7,7 @@ $host = 'localhost';
 
 $mysqli = new mysqli($host, $usuario, $senha, $database);
 
-if($mysqli->error){
-    die("falha na conexão com o banco de dados:" . $mysqli->error);
+if($mysqli->connect_error){
+    die("falha na conexão com o banco de dados:" . $mysqli->connect_error);
 } 
 
