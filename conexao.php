ae5ee82994c941c3ee9f3e49a21c8d08d2a20555
Victor Saymon <victor.vidal2@aluno.ce.gov.br>
2025-05-31 09:00:02 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $pdo = new PDO("mysql:host=localhost;dbname=login_no_seas", "root", "");
+        $pdo = new PDO("mysql:host=localhost;dbname="sistema_seas", "root", "");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $erro) {
         echo "Erro na conexão: " . $erro->getMessage();
