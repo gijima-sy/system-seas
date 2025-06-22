3e91fe184e911982f3ee19a46b4fc748d2a35bdf
Victor Saymon <victor.vidal2@aluno.ce.gov.br>
2025-06-22 19:31:00 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-    try {
-    $pdo = new PDO("mysql:host=localhost;dbname=sistema-seas", "root", "");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $erro) {
-        echo "Erro na conexão: " . $erro->getMessage();
-    }
-?>
